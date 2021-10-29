26fab68b83fc0f85e0c98c9af5a619ca7e7c9239
Edvaldo Junior <edonejunior@hotmail.com>
2021-10-29 13:17:33 -0300

    Cesta e Imgs

--- App.js
+++ App.js
@@ -1,21 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar, SafeAreaView } from 'react-native';
+import Cesta from './src/views/Cesta';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView>
+      <StatusBar />
+      <Cesta />
+    </SafeAreaView>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/views/Cesta.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import {StyleSheet, Image, Dimensions, Text, View} from 'react-native';
+
+import topo from '../../assets/topo.png';
+const width = Dimensions.get('screen').width;
+
+export default function Cesta() {
+    return <>
+
+        <Image source={topo} style={estilos.topo} />
+        <Text style={estilos.titulo}> Detalhe da cesta </Text>
+
+        <View style={estilos.cesta}>
+            <Text style={estilos.nome}>Cesta de Verduras</Text>
+            <Text>Jenny Jack Farm</Text>
+            <Text>Uma cesta com produtos selecionados cuidadosamente da fazenda direto para sua cozinha</Text>
+            <Text>R$ 40,00</Text>
+        </View>
+
+    </>
+}
+
+const estilos = StyleSheet.create({
+    topo:{
+        width: "100%",
+        height: 578 / 768 * width,
+    },
+    titulo:{
+        width: "100%",
+        position:"absolute",
+        textAlign:"center",
+        fontSize: 16,
+        lineHeight:26, 
+        color: "white",
+        fontWeight: "bold",
+        padding: 16,
+    },
+    cesta: {
+        paddingVertical: 8,
+        paddingHorizontal: 16,
+    },
+    nome:{
+        color:"#464646",
+        fontSize:26,
+        lineHeight:42,
+        fontWeight: "bold",
+    }
+})
+
+ 
+
