4df8c04e033af906c7f577c19ea8c178d9292e9c
Edvaldo Junior <edonejunior@hotmail.com>
2021-10-29 16:11:59 -0300

    Componente Texto e FontFamily

--- App.js
+++ App.js
@@ -1,8 +1,21 @@
 import React from 'react';
-import { StatusBar, SafeAreaView } from 'react-native';
+import { StatusBar, SafeAreaView, View } from 'react-native';
 import Cesta from './src/views/Cesta';
+import { useFonts, Montserrat_400Regular , Montserrat_700Bold } from '@expo-google-fonts/montserrat';
+
+
 
 export default function App() {
+
+  const [fonteCarregada] = useFonts({
+    "RobotoRegular": Montserrat_400Regular,
+    "RobotoBold": Montserrat_700Bold,
+  });
+
+  if(!fonteCarregada){
+    return <View></View>
+  }
+
   return (
     <SafeAreaView>
       <StatusBar />
--- src/views/Cesta.js
+++ src/views/Cesta.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import {StyleSheet, Image, Dimensions, Text, View} from 'react-native';
+import Texto from '../components/Texto';
+
+import logo from '../../assets/logo.png';
 
 import topo from '../../assets/topo.png';
 const width = Dimensions.get('screen').width;
@@ -8,13 +11,17 @@ export default function Cesta() {
     return <>
 
         <Image source={topo} style={estilos.topo} />
-        <Text style={estilos.titulo}> Detalhe da cesta </Text>
+        <Texto style={estilos.titulo}> Detalhe da cesta </Texto>
 
         <View style={estilos.cesta}>
-            <Text style={estilos.nome}>Cesta de Verduras</Text>
-            <Text>Jenny Jack Farm</Text>
-            <Text>Uma cesta com produtos selecionados cuidadosamente da fazenda direto para sua cozinha</Text>
-            <Text>R$ 40,00</Text>
+            <Texto style={estilos.nome}>Cesta de Verduras</Texto>
+            <View style={estilos.fazenda}>
+                <Image style={estilos.imagemFazenda} source={logo} />
+                <Texto style={estilos.nomeFazenda}>Jenny Jack Farm</Texto>
+            </View>
+            
+            <Texto style={estilos.descricao}>Uma cesta com produtos selecionados cuidadosamente da fazenda direto para sua cozinha</Texto>
+            <Texto style={estilos.preco}>R$ 40,00</Texto>
         </View>
 
     </>
@@ -39,11 +46,39 @@ const estilos = StyleSheet.create({
         paddingVertical: 8,
         paddingHorizontal: 16,
     },
+    nomeFazenda:{
+        fontSize:16,
+        lineHeight: 26,
+        marginLeft: 12,
+        fontWeight: "bold",
+    },
+    imagemFazenda:{
+        height: 32,
+        width: 32,
+        
+    },
+    fazenda:{
+        flexDirection: "row",
+        paddingVertical: 12,
+    },
     nome:{
         color:"#464646",
         fontSize:26,
         lineHeight:42,
         fontWeight: "bold",
+    },
+    descricao:{
+        color: "#A3A3A3",
+        fontSize: 16,
+        lineHeight: 26,
+        fontFamily: "RobotoRegular",
+    },
+    preco:{
+        color: "#2A9F85",
+        fontWeight: "bold",
+        fontSize: 26,
+        lineHeight:42,
+        marginTop: 8,
     }
 })
 
